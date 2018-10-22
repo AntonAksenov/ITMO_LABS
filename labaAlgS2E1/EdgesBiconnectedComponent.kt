adf9a9dd0042a7b73069359d82654d3dfd3e3ea9
AntonAksenov <37124085+AntonAksenov@users.noreply.github.com>
2018-10-23 01:38:39 +0300

    Update EdgesBiconnectedComponent.kt

--- labaAlgS2E1/EdgesBiconnectedComponent.kt
+++ labaAlgS2E1/EdgesBiconnectedComponent.kt
@@ -1,116 +1,91 @@
 package labaAlgS2E1
 
 import java.util.*
-import kotlin.collections.HashMap
 import kotlin.math.min
 
-lateinit var edges: Array<MutableList<Int>>
-lateinit var used: BooleanArray
-var t = 0
-lateinit var inTime: IntArray
-lateinit var backEdgeTime: IntArray
-lateinit var ans: BooleanArray
+object EdgesBiconnectedComponent {
+    lateinit var edges: Array<MutableList<Pair<Int, Int>>>
+    lateinit var isBridge: Array<Boolean>
+    lateinit var used: BooleanArray
+    var timer = 0
+    lateinit var inTime: IntArray
+    lateinit var maxBackEdge: IntArray
+    var compN = 0
+    lateinit var comp: IntArray
 
-var maxComponent = 0
-val edgesMap = HashMap<Pair<Int, Int>, Int>()
-lateinit var component: IntArray
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        edges = Array(n) { mutableListOf<Pair<Int, Int>>() }
 
-
-fun main(args: Array<String>) {
-    val sc = Scanner(System.`in`)
-    val n = sc.nextInt()
-    val m = sc.nextInt()
-    edges = Array(n) { mutableListOf<Int>() }
-    used = BooleanArray(n)
-    ans = BooleanArray(n)
-    t = 0
-    inTime = IntArray(n)
-    backEdgeTime = IntArray(n)
-    component = IntArray(m) { -1 }
-
-    for (i in 0 until m) {
-        val v = sc.nextInt() - 1
-        val u = sc.nextInt() - 1
-        edges[u].add(v)
-        edges[v].add(u)
-        edgesMap[Pair(v, u)] = i
-        edgesMap[Pair(u, v)] = i
-    }
-
-    for (i in 0 until n) {
-        if (!used[i]) {
-            if (dfs(i, -1) > 1) {
-                ans[i] = true
+        val m = sc.nextInt()
+        isBridge = Array(m + 1) { false }
+        for (i in 0 until m) {
+            var v = sc.nextInt() - 1
+            var u = sc.nextInt() - 1
+            edges[u].add(Pair(v, i + 1))
+            edges[v].add(Pair(u, i + 1))
+        }
+        used = BooleanArray(n) { false }
+        inTime = IntArray(n) { 0 }
+        maxBackEdge = IntArray(n) { 0 }
+        for (i in 0 until n) {
+            if (!used[i]) {
+                bridgesDfs(i, -1, -1)
             }
         }
-    }
-    used = BooleanArray(n)
-    for (v in 0 until n) {
-        if (!used[v]) {
-            maxComponent++
-            paint(v, maxComponent, -1)
+
+        timer = 0
+        used = BooleanArray(n) { false }
+
+        comp = IntArray(n) { 0 }
+        for (i in 0 until n) {
+            if (!used[i]) {
+                comp[i] = ++compN
+                compDfs(i, compN)
+            }
+        }
+        println(compN)
+        for (i in 0 until n) {
+            print("${comp[i]} ")
         }
     }
 
-    val componentNumber = IntArray(n) { -1 }
-    var componentCounter = 0
-
-    for (i in 0 until m) {
-        val comp = componentNumber[component[i]]
-        if (comp == -1) {
-            componentNumber[component[i]] = ++componentCounter
-            component[i] = componentCounter
-        } else {
-            component[i] = comp
-        }
-    }
-
-    println(componentCounter)
-    for (i in 0 until component.size) {
-        print("${component[i]} ")
-    }
-
-}
-
-fun paint(v: Int, color: Int, parent: Int) {
-    used[v] = true
-    for (u in edges[v]) {
-        if (u != parent) {
-            if (!used[u]) {
-                if (backEdgeTime[u] >= inTime[v]) {
-                    component[edgesMap[Pair(v, u)]!!] = ++maxComponent
-                    paint(u, maxComponent, v)
+    fun bridgesDfs(v: Int, from: Int = -1, lastEdge: Int = -1) {
+        used[v] = true
+        inTime[v] = timer++
+        maxBackEdge[v] = inTime[v]
+        for ((u, e) in edges[v]) {
+            if (u != from || e != lastEdge) {
+                if (used[u]) {
+                    maxBackEdge[v] = min(maxBackEdge[v], inTime[u])
                 } else {
-                    component[edgesMap[Pair(v, u)]!!] = color
-                    paint(u, color, v)
+                    bridgesDfs(u, v, e)
+                    maxBackEdge[v] = min(maxBackEdge[v], maxBackEdge[u])
+                    if (maxBackEdge[u] > inTime[v]) {
+                        isBridge[e] = true
+                    }
                 }
-            } else if (inTime[u] < inTime[v]) {
-                component[edgesMap[Pair(v, u)]!!] = color
             }
         }
     }
-}
 
-fun dfs(v: Int, p: Int): Int {
-    var ch = 0
-    used[v] = true
-    backEdgeTime[v] = t++
-    inTime[v] = backEdgeTime[v]
-    for (u in edges[v]) {
-        if (u != p) {
-            if (used[u]) {
-                backEdgeTime[v] = min(backEdgeTime[v], inTime[u])
+    fun compDfs(v: Int, counter: Int) {
+        used[v] = true
+        for ((u, e) in edges[v]) {
+            if (used[u]) continue
+            if (isBridge[e]) {
+                compN++
+                comp[u] = compN
+                compDfs(u, compN)
             } else {
-                dfs(u, v)
-                backEdgeTime[v] = min(backEdgeTime[v], backEdgeTime[u])
-                if (backEdgeTime[u] >= inTime[v] && p != -1) {
-                    ans[v] = true
-                }
-                ch++
+                comp[u] = counter
+                compDfs(u, counter)
             }
         }
     }
-    return ch
+
 }
 
 //6 7
