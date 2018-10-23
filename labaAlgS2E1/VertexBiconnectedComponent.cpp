1721f2412e5fd2883baed8bbe56844e577244b8d
AntonAksenov <37124085+AntonAksenov@users.noreply.github.com>
2018-10-23 18:36:47 +0300

    Update VertexBiconnectedComponent.cpp

--- labaAlgS2E1/VertexBiconnectedComponent.cpp
+++ labaAlgS2E1/VertexBiconnectedComponent.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <map>
 
-static std::vector<std::vector<int> > edges;
+static std::vector<std::vector<std::pair<int, int>>> edges;
 static std::vector<bool> used;
 static int t = 0;
 static std::vector<int> inTime;
@@ -10,37 +10,40 @@ static std::vector<int> backEdgeTime;
 static std::vector<bool> ans;
 
 static int maxComponent = 0;
-static std::map<std::pair<int, int>, int> edgesMap;
 static std::vector<int> component;
 
-void paint(int v, int color, int parent) {
+void paint(int v, int color, int e) {
     used[v] = true;
-    for (int u : edges[v]) {
-        if (u != parent) {
+    for (std::pair pair : edges[v]) {
+        int u = pair.first;
+        int k = pair.second;
+        if (k != e) {
             if (!used[u]) {
                 if (backEdgeTime[u] >= inTime[v]) {
-                    component[edgesMap[std::pair<int, int>(v, u)]] = ++maxComponent;
-                    paint(u, maxComponent, v);
+                    component[k] = ++maxComponent;
+                    paint(u, maxComponent, k);
                 } else {
-                    component[edgesMap[std::pair<int, int>(v, u)]] = color;
-                    paint(u, color, v);
+                    component[k] = color;
+                    paint(u, color, k);
                 }
             } else if (inTime[u] < inTime[v]) {
-                component[edgesMap[std::pair<int, int>(v, u)]] = color;
+                component[k] = color;
             }
         }
     }
 }
 
-void dfs(int v, int p) {
+void dfs(int v, int e) {
     used[v] = true;
     inTime[v] = backEdgeTime[v] = t++;;
-    for (int u : edges[v]) {
-        if (u != p) {
+    for (std::pair pair : edges[v]) {
+        int u = pair.first;
+        int k = pair.second;
+        if (k != e) {
             if (used[u]) {
                 backEdgeTime[v] = std::min(backEdgeTime[v], inTime[u]);
             } else {
-                dfs(u, v);
+                dfs(u, k);
                 backEdgeTime[v] = std::min(backEdgeTime[v], backEdgeTime[u]);
             }
         }
@@ -61,10 +64,8 @@ int main() {
     for (int i = 0; i < m; ++i) {
         int v, u;
         std::cin >> v >> u;
-        edges[u - 1].push_back(v - 1);
-        edges[v - 1].push_back(u - 1);
-        edgesMap[std::pair<int, int>(v - 1, u - 1)] = i;
-        edgesMap[std::pair<int, int>(u - 1, v - 1)] = i;
+        edges[u - 1].push_back(std::make_pair(v - 1, i));
+        edges[v - 1].push_back(std::make_pair(u - 1, i));
     }
 
     for (int i = 0; i < n; ++i) {
@@ -75,28 +76,24 @@ int main() {
     used.assign(n, false);
     for (int i = 0; i < n; ++i) {
         if (!used[i]) {
-            maxComponent++;
             paint(i, maxComponent, -1);
         }
     }
 
-    std::vector<int> componentNumber;
-    componentNumber.assign(maxComponent + 1, -1);
-    int componentCounter = 0;
-
-    for (int i = 0; i < m; ++i) {
-        int comp = componentNumber[component[i]];
-        if (comp == -1) {
-            componentNumber[component[i]] = ++componentCounter;
-            component[i] = componentCounter;
-        } else {
-            component[i] = comp;
-        }
-    }
-
-    std::cout << componentCounter << std::endl;
+    std::cout << maxComponent << std::endl;
     for (int i = 0; i < m; ++i) {
         std::cout << component[i] << " ";
     }
     return 0;
 }
+
+//5 6
+//1 2
+//2 3
+//3 1
+//1 4
+//4 5
+//5 1
+
+//2
+//1 1 1 2 2 2 
